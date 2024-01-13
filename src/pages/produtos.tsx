314abe489cb830725ce5087983fa49996989c996
Maycon Lopes <mayconblopes@gmail.com>
2024-01-13 10:28:43 -0300

    style: UX and UI updated

--- src/pages/produtos.tsx
+++ src/pages/produtos.tsx
@@ -1,10 +1,45 @@
 import React from 'react'
 import Layout from '../components/Layout'
 
+import * as styles from './index.module.css'
+import { Box } from '@mui/system'
+
 export default function Produtos() {
   return (
     <Layout>
-        {/* embed from canvas */}
+      <Box
+      position='absolute'
+      zIndex='5'
+      top='70px'
+        display='flex'
+        flexDirection='column'
+        justifyContent='center'
+        alignItems='center'
+        margin='0 auto'
+        gap='15px'
+        width='calc(min(100%,600px))'
+        boxSizing='border-box'
+        sx={{
+        //   background: 'white',
+          background: 'linear-gradient(135deg, #2AFADF 0%, #4C83FF 100%)',
+          // background: 'linear-gradient(135deg, #FFF3B0 0%, #CA26FF 100%)',
+
+          padding: '15px',
+        }}
+      >
+        <p className={styles.specialFont2}>
+          {' '}
+          Fale com a gente no{' '}
+          <a
+            className={styles.almostAButton}
+            target='_blank'
+            href='https://wa.me/5522996026139'
+          >
+            WhatsApp
+          </a>
+        </p>
+      </Box>
+      {/* embed from canvas */}
       <div
         style={{
           position: 'relative',
@@ -19,7 +54,7 @@ export default function Produtos() {
             position: 'absolute',
             width: '100%',
             height: '100%',
-            top: '0',
+            top: '50px',
             left: '0',
             border: 'none',
             padding: '0',
