1adbe7abab9a2187a71dd72f48a33db3006666e5
Maycon Lopes <mayconblopes@gmail.com>
2024-10-05 08:59:11 -0300

    new arts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,7 +36,7 @@ export default function Fotos({ data }: any) {
 
   const [open, setOpen] = useState(false)
   const [img, setImg] = useState({} as FluidObject)
-  const [filter, setFilter] = useState('Dia dos Pais')
+  const [filter, setFilter] = useState('Dia do Professor')
   const [filteredFotos, setFilteredFotos] = useState(
     fotos.filter((foto: any) => foto.relativePath.includes(filter))
   )
